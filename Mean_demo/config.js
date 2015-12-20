2e8847cbe5471496a8970c426e1d7737b10cd46f
Rahool <rahool@contrailtech.com>
2015-12-20 11:46:27 +0530

    mongo database setting

--- Mean_demo/config.js
+++ Mean_demo/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"database" : "mongodb://xxxx:xxxxx@ds045704.mongolab.com:45704/grocery",
+	"database" : "mongodb://admin:admin123@ds045704.mongolab.com:45704/grocery",
 	"port" : process.env.PORT || 4000,
 	"secretKey" : "SecreKeyIsalwaysNeedToBeSecret"
 };
